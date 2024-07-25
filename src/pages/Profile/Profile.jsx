de1292a4cb665fc816a59944674db64f6e5952e3
aicholpondev <aicholponsultanii@gmail.com>
2024-07-25 13:16:07 +0600

    profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -5,6 +5,18 @@ export default function  Profile(){
         <Navigation/>
     <div className={style.profileCon}>
         <h1>Profile</h1>
+        <div>
+            <h2>29</h2>
+            <p>recipe</p>
+        </div>
+        <div>
+            <h2>144</h2>
+            <p>followers</p>
+        </div>
+        <div>
+            <h2>118</h2>
+            <p>follow</p>
+        </div>
     </div>
     </>
 }
